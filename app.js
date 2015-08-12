c41d29368ede8c33265af5cc962244bf268f88db
Ivan Aguilar <ivanedu49@gmail.com>
2015-08-12 18:59:54 -0500

    autentificacion y sesion

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var partials = require('express-partials');
 var routes = require('./routes/index');
 var app = express();
 var methodOverride=require('method-override');
-
+var session=require('express-session');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,9 +20,23 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Helpers dinamicos
+app.use(function(req,res,next){
+    if(!req.path.match(/\/login|\/logout/)){
+        req.session.redir=req.path;
+    }
+    //hacer visible re.session en las vistas
+    res.locals.session=req.session;
+    next();
+});
+
+
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController=require('../controllers/quiz_controller');
 var commentController=require('../controllers/comment_controller');
+var sessionController=require('../controllers/session_controller');
 /* GET home page. */ 
 router.get('/', function(req, res) {
 	res.render('index', { title: 'Quiz',errors:[] });
@@ -10,6 +11,11 @@ router.get('/', function(req, res) {
 
 //autoload de comandos con quizId
 router.param('quizId',quizController.load);
+
+//definicion de rutas de sesion
+router.get('/login',sessionController.new);
+router.post('/login',sessionController.create);
+router.get('/logout',sessionController.destroy);
 
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
@@ -22,6 +28,7 @@ router.put('/quizes/:quizId(\\d+)',quizController.update);
 router.delete('/quizes/:quizId(\\d+)',quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
 
 module.exports = router;
