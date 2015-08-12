f4cec8f5dd452e50623a0f33efe7aa57fdc3dc79
Ivan Aguilar <ivanedu49@gmail.com>
2015-08-11 19:08:28 -0500

    editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,7 @@ router.get('/author',function(req, res) {res.render('author',{errors:[]})});
 router.get('/quizes',quizController.index);
 router.get('/quizes/new',quizController.new);
 router.post('/quizes/create',quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
+router.put('/quizes/:quizId(\\d+)',quizController.update);
 
 module.exports = router;
