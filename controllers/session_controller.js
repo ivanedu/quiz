1fc62527df948bd7f1643b34c2935f782559d096
Ivan Aguilar <ivanedu49@gmail.com>
2015-08-12 20:13:56 -0500

    autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -23,3 +23,11 @@ exports.destroy=function(req,res){
 	delete req.session.user;
 	res.redirect(req.session.redir.toString());
 }
+
+exports.loginRequired=function(req,res){
+	if(req.session.user){
+		next();		
+	}else{
+		res.redirect('/login');
+	}
+};
